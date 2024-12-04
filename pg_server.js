8567234c7cb0544dcf69f854e512c137aae95c26
ojalamiika <n2ojmi00@students.oamk.fi>
2024-12-04 10:57:42 +0200

    Movie adding to database

--- pg_server.js
+++ pg_server.js
@@ -1,29 +1,43 @@
 import express from "express";
-import pg from "pg"
-import dotenv from "dotenv"
+import {pgPool} from './pg_connection.js'
 
-dotenv.config()
+// dotenv.config()
 
 const app = express();
-
-const {Client} = pg
+app.use(express.json())
 
 app.listen(3001, () => {
     console.log('Running!n')
 })
 
-const client = new Client()
 
-connect()
-
-async function connect() {
-   // await client.connect()
-
+// Dummy response
+app.get('/movies', (req,res) => {
+    res.json([
+        { id: 1, title: 'Inception', year: 2010 },
+        { id: 2, title: 'Interstellar', year: 2014 }
+    ])
+})
     try {
-        await client.connect()
-        await client.query("INSERT INTO company VALUES ('OAMK', 'Biologiantie 12', 'Oulu')")
+        await pgPool.connect()
         console.log('Database connected')
+        await pgPool.query("INSERT INTO company VALUES ('Kastellin lukio', 'Sairaalantie 14', 'Oulu')")
+        .then(console.log("Successful"))
+        
     } catch (error) {
         console.log(error)
     }
-}
+
+app.get('/movie', async (req,res) => {
+    try{
+        const result = await pgPool.query('SELECT * FROM company')
+        res.json(result.rows)
+        console.log(result)
+    } catch(e) {
+        console.log(e);
+    }
+})
+
+
+
+
--- pg_testing.js
+++ pg_testing.js
@@ -1,7 +1,18 @@
 import {pgPool} from './pg_connection.js'
 
+const mName = 'Hekan fragmovie'
+const mYear = '2024'
+const mGenre = 'Rakkaus'
+const mKeywords = 'Sekopää'
+
 try{
-    await pgPool.query('')
+    //const result = await pgPool.query("INSERT INTO movie (movie_id, movie_name, year, genre, keywords) VALUES ('1', 'Matin_elokuva', '2022', 'Kauhu', 'Pelottava')")
+    const result = await pgPool.query('SELECT * FROM movie')
+    const mId = result.rowCount + 1
+    console.log(mId)
+    
+    const results = await pgPool.query("INSERT INTO movie VALUES ($1, $2, $3, $4, $5)", [mId, mName, mYear, mGenre, mKeywords, ])
+    console.log(results)
 } catch(e) {
     console.log(e.message)
 }
